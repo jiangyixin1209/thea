a0a0631d690a26c223b392368c29a06c46553bd7
jiangyixin1209 <17602173915@163.com>
2021-01-14 16:02:29 +0800

    测试样例

--- thea-sample/thea-sample-web/src/main/java/top/jiangyixin/thea/sample/web/config/TheaConfig.java
+++ thea-sample/thea-sample-web/src/main/java/top/jiangyixin/thea/sample/web/config/TheaConfig.java
@@ -1,10 +1,49 @@
 package top.jiangyixin.thea.sample.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.jiangyixin.thea.core.TheaSsoClient;
+import top.jiangyixin.thea.core.common.SsoConfig;
+import top.jiangyixin.thea.core.filter.SsoWebFilter;
+
 /**
  * TODO
  * @version 1.0
  * @author jiangyixin
  * @date 2021/1/14 上午11:14
  */
+@Configuration
 public class TheaConfig {
+	@Value("${thea.sso.server}")
+	private String ssoServer;
+	@Value("${thea.sso.redis-address}")
+	private String ssoRedisAddress;
+	@Value("${thea.sso.excluded-path}")
+	private String ssoExcludedPath;
+	@Value("${thea.sso.logout-path}")
+	private String ssoLogoutPath;
+	
+	@Bean
+	public FilterRegistrationBean<SsoWebFilter> ssoWebFilterRegistration() {
+		TheaSsoClient.init(ssoRedisAddress);
+		
+		// filter init
+		FilterRegistrationBean<SsoWebFilter> registration = new FilterRegistrationBean<>();
+		
+		// 设置过滤器名称
+		registration.setName("XxlSsoWebFilter");
+		// 设置过滤器优先级，越低越优先，最顶级为1
+		registration.setOrder(1);
+		// 过滤所有路径
+		registration.addUrlPatterns("/*");
+		// 注册自定义过滤器
+		registration.setFilter(new SsoWebFilter());
+		// 配置初始化参数
+		registration.addInitParameter(SsoConfig.SSO_SERVER, ssoServer);
+		registration.addInitParameter(SsoConfig.SSO_LOGOUT_PATH, ssoLogoutPath);
+		registration.addInitParameter(SsoConfig.SSO_EXCLUDED_PATHS, ssoExcludedPath);
+		return registration;
+	}
 }
--- thea-sample/thea-sample-web/src/main/java/top/jiangyixin/thea/sample/web/controller/IndexController.java
+++ thea-sample/thea-sample-web/src/main/java/top/jiangyixin/thea/sample/web/controller/IndexController.java
@@ -1,10 +1,36 @@
 package top.jiangyixin.thea.sample.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.jiangyixin.thea.core.common.SsoConfig;
+import top.jiangyixin.thea.core.domain.SsoUser;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * TODO
  * @version 1.0
  * @author jiangyixin
  * @date 2021/1/14 下午3:55
  */
+@Controller
 public class IndexController {
+	
+	@GetMapping("/")
+	public String index(Model model, HttpServletRequest request) {
+		
+		SsoUser ssoUser = (SsoUser) request.getAttribute(SsoConfig.SSO_USER);
+		model.addAttribute("ssoUser", ssoUser);
+		return "index";
+	}
+	
+	@GetMapping("/json")
+	@ResponseBody
+	public SsoUser json(Model model, HttpServletRequest request) {
+		SsoUser ssoUser = (SsoUser) request.getAttribute(SsoConfig.SSO_USER);
+		model.addAttribute("ssoUser", ssoUser);
+		return ssoUser;
+	}
 }
